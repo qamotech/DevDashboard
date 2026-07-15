6ae925103ca026ba82e370d207606dfb724c2130
qamotech <qamotech@gmail.com>
2026-07-15 17:20:12 -0400

    Add funPromptz creative media arcade

--- build-site.mjs
+++ build-site.mjs
@@ -4,5 +4,7 @@ await rm("dist", { recursive: true, force: true });
 await mkdir("dist/client", { recursive: true });
 await mkdir("dist/server", { recursive: true });
 await cp("n8Prompt.html", "dist/client/n8Prompt.html");
+await cp("funPromptz.html", "dist/client/funPromptz.html");
+await cp("funpromptz-og.png", "dist/client/funpromptz-og.png");
 await cp("n8-icon.png", "dist/client/n8-icon.png");
 await cp("worker/index.js", "dist/server/index.js");
